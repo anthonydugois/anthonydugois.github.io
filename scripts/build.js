a5c4b62415d3e2feba11e046574733259aa9fe59
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-09-17 15:27:07 +0200

    init

---
+++ scripts/build.js
@@ -0,0 +1,37 @@
+import webpack from "webpack"
+import config from "../webpack.config"
+
+import colors from "chalk"
+import opn from "opn"
+import server from "./server"
+import {defineVariables} from "../variables"
+
+defineVariables()
+
+if (__PROD__) {
+    webpack(config, (err) => {
+        if (err) {
+            throw err
+        }
+
+        console.log(colors.green(
+            `\n✓ Build successfully completed`
+        ))
+    })
+} else {
+    server({
+        __OUTPUT_DIR__,
+        __SERVER_PORT__,
+        __SERVER_HOST__,
+    }, (err) => {
+        if (err) {
+            throw err
+        }
+
+        opn(__SERVER_URL__)
+
+        console.log(colors.green(
+            `\n✓ Server started at ${__SERVER_URL__}`
+        ))
+    })
+}
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+import path from "path"
+import webpack from "webpack"
+import ExtractTextPlugin from "extract-text-webpack-plugin"
+
+import variables, {defineVariables} from "./variables"
+
+defineVariables()
+
+export default {
+    entry: {
+        index: [
+            "./src/index.js",
+        ],
+    },
+    output: {
+        path: path.join(__dirname, __OUTPUT_DIR__),
+        publicPath: `${__SERVER_URL__}/${__OUTPUT_DIR__}/`,
+        filename: `bundle.js`,
+    },
+    resolve: {
+        extensions: [
+            "",
+            ".js",
+            ".css",
+        ],
+    },
+    module: {
+        loaders: [
+            // js files
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: [
+                    "babel?stage=0",
+                    "eslint",
+                ],
+            },
+            // css files
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract(
+                    "style",
+                    "css"
+                ),
+            },
+        ],
+    },
+    plugins: [
+        new webpack.DefinePlugin(variables),
+        new ExtractTextPlugin("styles.css", {
+            disable: __DEV__,
+        }),
+        ...(__PROD__ ? [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                },
+            }),
+        ] : [])
+    ],
+    eslint: {
+        configFile: "./.eslintrc",
+    },
+}
