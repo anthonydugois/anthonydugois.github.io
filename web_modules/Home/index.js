94547f8234df593639dbffa4d3ff84b5e23f079e
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-09-28 20:57:57 +0200

    optim code

--- web_modules/Home/index.js
+++ web_modules/Home/index.js
@@ -1,5 +1,4 @@
 import "./styles"
-import getCubesMatrix from "./cubesMatrix"
 
 import THREE from "three"
 import oc from "three-orbit-controls"
@@ -32,47 +31,63 @@ document.querySelector("#three").appendChild(RENDERER.domElement)
 CAMERA.position.set(0, 150, 150)
 CAMERA.lookAt(new THREE.Vector3())
 
+SCENE.add(new THREE.AxisHelper(500))
+
 // let's draw the scene!
-const o = new THREE.Object3D()
+const bigCube = new THREE.Object3D()
 
-const lightX = new THREE.DirectionalLight(0xeeeeee, 1),
-    lightX2 = lightX.clone(),
+// lights
+/*const lightX = new THREE.DirectionalLight(0xeeeeee, 1),
     lightY = new THREE.DirectionalLight(0xdddddd, 1),
-    lightY2 = lightY.clone(),
-    lightZ = new THREE.DirectionalLight(0xcccccc, 1),
-    lightZ2 = lightZ.clone()
+    lightZ = new THREE.DirectionalLight(0xcccccc, 1)
 
-lightX.position.set(100, 0, 0)
-lightY.position.set(0, 100, 0)
-lightZ.position.set(0, 0, 100)
+lightX.position.x = 100
+lightY.position.y = 100
+lightZ.position.z = 100
 
-lightX2.position.set(-100, 0, 0)
-lightY2.position.set(0, -100, 0)
-lightZ2.position.set(0, 0, -100)
-
-o.add(lightX)
-o.add(lightY)
-o.add(lightZ)
-
-o.add(lightX2)
-o.add(lightY2)
-o.add(lightZ2)
-
-// dimensions
-const size = 5,
-    gap = .2,
-    d = size + gap
+bigCube.add(lightX)
+bigCube.add(lightY)
+bigCube.add(lightZ)*/
 
 // objects
-const cubes = new THREE.Object3D(),
-    cube = new THREE.Mesh(
-        new THREE.BoxGeometry(size, size, size),
-        new THREE.MeshLambertMaterial()
-    )
+const length = 3, size = 5,
+    cubes = new THREE.Object3D(),
+    cube = new THREE.Mesh(new THREE.BoxGeometry(size, size, size), new THREE.MeshLambertMaterial())
 
 // matrixes
-let cubesMatrix = getCubesMatrix(d),
+let cubesMatrix = getCube({ size, length }),
     randomCube = THREE.Math.randInt(0, cubesMatrix.length - 1)
+
+function getCube({ size, length }) {
+    let vectors = [],
+        gap = .2,
+        d = size + gap,
+        half = Math.floor(length / 2)
+
+    for (let f = 0 ; f < length ; f++) {
+        let lines = []
+
+        for (let l = 0 ; l < length ; l++) {
+            let cols = []
+
+            for (let c = 0 ; c < length ; c++) {
+                let x = (c - half) * d,
+                    y = (l - half) * d,
+                    z = (f - half) * d
+
+                cols.push(new THREE.Vector3(x, y, z))
+            }
+
+            if (l % 2 === 0) lines.push(...cols)
+            else lines.push(...cols.reverse())
+        }
+
+        if (f % 2 === 0) vectors.push(...lines)
+        else vectors.push(...lines.reverse())
+    }
+
+    return vectors
+}
 
 // define cubes
 cubesMatrix.forEach((coords, index) => {
@@ -80,31 +95,27 @@ cubesMatrix.forEach((coords, index) => {
 
     cubes.add(_cube)
 
-    _cube.position.x = coords.x
-    _cube.position.y = coords.y
-    _cube.position.z = coords.z
+    _cube.position.set(coords.x, coords.y, coords.z)
 })
 
-o.add(cubes)
+bigCube.add(cubes)
 
-o.applyMatrix(new THREE.Matrix4().makeRotationY(PI / 4))
-
-SCENE.add(o)
+SCENE.add(bigCube)
 
 // udate the scene
-let toRight = true
+update(true, .5)
 
-update()
-
-function update(spd = .5) {
+function update(toRight, spd) {
     for (let cubesCount = cubes.children.length, emptyIndex = 0 ; emptyIndex < cubesCount ; emptyIndex++) {
         if (cubes.children[emptyIndex].type === "Object3D") {
             let cubeIndex
 
-            if (toRight) {
-                cubeIndex = emptyIndex + 1
-            } else {
-                cubeIndex = emptyIndex - 1
+            if (toRight) cubeIndex = emptyIndex + 1
+            else cubeIndex = emptyIndex - 1
+
+            if (cubeIndex < 0) {
+                cubeIndex = 0
+                toRight = true
             }
 
             if (cubeIndex > cubesCount - 1) {
@@ -112,35 +123,22 @@ function update(spd = .5) {
                 toRight = false
             }
 
-            if (cubeIndex < 0) {
-                cubeIndex = 0
-                toRight = true
-            }
-
             const empty = cubes.children[emptyIndex],
-                cube = cubes.children[cubeIndex]
+                cube = cubes.children[cubeIndex],
+                _cube = cube.clone()
 
             cubes.children[emptyIndex] = cube
             cubes.children[cubeIndex] = empty
 
-            const fakeEmpty = empty.clone(),
-                fakeCube = cube.clone()
+            TweenLite.to(cube.position, spd, { ...empty.position })
 
-            TweenLite.to(cube.position, spd, {
-                x: fakeEmpty.position.x,
-                y: fakeEmpty.position.y,
-                z: fakeEmpty.position.z,
-            })
-
-            empty.position.x = fakeCube.position.x
-            empty.position.y = fakeCube.position.y
-            empty.position.z = fakeCube.position.z
+            empty.position.set(_cube.position.x, _cube.position.y, _cube.position.z)
 
             break
         }
     }
 
-    setTimeout(update, 1000 * spd)
+    setTimeout(() => update(toRight, spd), 1000 * spd)
 }
 
 window.onmousemove = (e) => {
@@ -151,7 +149,7 @@ window.onmousemove = (e) => {
         halfY = y / 2,
         halfH = HEIGHT / 2
 
-    TweenLite.to(o.rotation, 1, {
+    TweenLite.to(bigCube.rotation, 1, {
         x: (halfY / halfH) * PI,
         y: -(halfX / halfW) * PI,
     })
