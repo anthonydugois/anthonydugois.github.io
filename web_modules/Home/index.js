79db46028b9ecc6fd3b6c90e2a25bb1027b5ad9e
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-11-01 19:42:29 +0100

    prod

--- web_modules/Home/index.js
+++ web_modules/Home/index.js
@@ -9,8 +9,8 @@ const rand = (min, max) => min + Math.random() * (max - min)
 const randInt = (min, max) => Math.round(rand(min, max))
 
 const colors = {
-    primary: 0x000000,
-    secondary: 0x2196F3,
+    ambient: 0x000000,
+    global: 0x607D8B,
 }
 
 // init the scene
@@ -19,7 +19,7 @@ let WIDTH = window.innerWidth,
     RATIO = WIDTH / HEIGHT,
     FIELDVIEW = 60,
     NEAR = 1,
-    FAR = 5000,
+    FAR = 1000,
     MOUSE = new THREE.Vector2()
 
 const CAMERA = new THREE.PerspectiveCamera(FIELDVIEW, RATIO, NEAR, FAR)
@@ -36,64 +36,46 @@ CAMERA.position.set(0, 0, 200)
 CAMERA.lookAt(new THREE.Vector3())
 
 // draw the scene
-const nbBoxes = 100
-const minSize = 1, maxSize = 10
-const boxSize = (maxSize - minSize) / nbBoxes
+const nbBoxes = 100,
+    minSize = 1,
+    maxSize = 10,
+    boxSize = (maxSize - minSize) / nbBoxes
 
 const rad = 100, a = .02
 
 const boxes = new THREE.Object3D()
 const boxMat = new THREE.MeshLambertMaterial()
 
-const spheres = []
-const sphMat = new THREE.MeshBasicMaterial({ color: colors.secondary })
-
 for (let i = nbBoxes ; i > 0 ; i--) {
     const size = minSize + boxSize * i
-    const posY = size * 2 + randInt(-10, 10)
 
-    const boxGeom = new THREE.BoxGeometry(size, size, size)
     const boxParent = new THREE.Object3D()
+    const boxGeom = new THREE.BoxGeometry(size, size, size)
     const box = new THREE.Mesh(boxGeom, boxMat)
 
+    boxParent.add(box)
     box.position.y = randInt(rad - size, rad + size)
     box.position.z = randInt(-size, size)
 
-    const sphGeom = new THREE.SphereGeometry(rand(.1, .6), 4, 4)
-    const sphParent = new THREE.Object3D()
-    const sph = new THREE.Mesh(sphGeom, sphMat)
-    const sphLight = new THREE.PointLight(colors.secondary, .15, posY)
-
-    sph.position.y = posY
-    sphLight.position.y = posY
-
-    sphParent.add(sph)
-    sphParent.add(sphLight)
-    sphParent.position.y = box.position.y
-    sphParent.rotation.x = rand(0, PI)
-
-    spheres.push(sphParent)
-
-    boxParent.add(box)
-    boxParent.add(sphParent)
-    boxParent.rotation.z = i * a
-
     boxes.add(boxParent)
+    boxParent.rotation.z = i * a
 }
 
 const lights = new THREE.Object3D()
-const ambientLight = new THREE.AmbientLight(colors.primary)
+const ambientLight = new THREE.AmbientLight(colors.ambient)
+const globalLight = new THREE.PointLight(colors.global, 1, rad + 30)
 
 lights.add(ambientLight)
+lights.add(globalLight)
 
-SCENE.add(lights)
 SCENE.add(boxes)
+SCENE.add(lights)
 
 // update the scene
 function update() {
     boxes.children.forEach((box, index, _boxes) => {
         if (index === 0 || _boxes[index - 1].rotation.z > a) {
-            box.rotation.z += .01
+            box.rotation.z += .015
         }
 
         if (index === 0 || _boxes[index - 1].rotation.x > .005) {
@@ -102,11 +84,9 @@ function update() {
 
         let sign = index % 2 === 0 ? 1 : -1
 
-        box.children[0].rotation.x += sign * rand(.01, .05)
-        box.children[0].rotation.y += sign * rand(.01, .05)
+        box.children[0].rotation.x += sign * rand(.01, .03)
+        box.children[0].rotation.y += sign * rand(.01, .03)
     })
-
-    spheres.forEach((sph, index, _spheres) => sph.rotation.x += (index + 1) / _spheres.length * .05)
 }
 
 window.onmousemove = (e) => {
@@ -115,7 +95,7 @@ window.onmousemove = (e) => {
 
     TweenLite.to(CAMERA.rotation, 1, {
         x: MOUSE.y * PI / 32,
-        y: -MOUSE.x * PI / 32,
+        y: - MOUSE.x * PI / 32,
     })
 }
 
