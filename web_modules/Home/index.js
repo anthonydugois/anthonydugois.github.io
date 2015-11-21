6edf8a088cdf35d03a795525af4699cad947b95a
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-11-21 15:03:16 +0100

    Update styles

--- web_modules/Home/index.js
+++ web_modules/Home/index.js
@@ -8,9 +8,11 @@ const rand = (min, max) => min + Math.random() * (max - min)
 const randInt = (min, max) => Math.round(rand(min, max))
 
 const colors = {
-  ambient: 0x000000,
-  global: 0x607D8B,
+  ambient: 0x050505,
+  global: 0x00BCD4,
 }
+
+const c = document.querySelector(".js-Canvas")
 
 // init the scene
 let WIDTH = window.innerWidth,
@@ -20,26 +22,29 @@ let WIDTH = window.innerWidth,
   NEAR = 1,
   FAR = 1000
 
+c.width = WIDTH
+c.height = HEIGHT
+
 const CAMERA = new THREE.PerspectiveCamera(FIELDVIEW, RATIO, NEAR, FAR)
 const SCENE = new THREE.Scene()
 const RENDERER = new THREE.WebGLRenderer({
   alpha: true,
   antialias: true,
-  canvas: document.querySelector(".js-Canvas"),
+  canvas: c,
 })
 
 RENDERER.setSize(WIDTH, HEIGHT)
 
-CAMERA.position.set(0, 0, 200)
+CAMERA.position.set(0, 0, 180)
 CAMERA.lookAt(new THREE.Vector3())
 
 // draw the scene
-const nbBoxes = 100,
+const nbBoxes = 250,
   minSize = 1,
   maxSize = 10,
   boxSize = (maxSize - minSize) / nbBoxes
 
-const rad = 90, a = .02
+const rad = 90, a = .08, vZ = .015, vX = .002
 
 const boxes = new THREE.Object3D()
 const boxMat = new THREE.MeshLambertMaterial()
@@ -73,13 +78,17 @@ SCENE.add(lights)
 function update() {
   boxes.children.forEach((box, index, _boxes) => {
     if (index === 0 || _boxes[index - 1].rotation.z > a) {
-      box.rotation.z += .015
+      box.rotation.z += vZ
+    }
+
+    if (index === 0 || _boxes[index - 1].rotation.x > vX) {
+      box.rotation.x += vX
     }
 
     let sign = index % 2 === 0 ? 1 : -1
 
-    box.children[0].rotation.x += sign * rand(.01, .03)
-    box.children[0].rotation.y += sign * rand(.01, .03)
+    box.children[0].rotation.x += sign * rand(.01, .04)
+    box.children[0].rotation.y += sign * rand(.01, .04)
   })
 }
 
