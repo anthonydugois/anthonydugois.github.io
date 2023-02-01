477365b08a70cc295e6c5eaeff437a4f9525366a
Anthony Dugois <anthony.dugois@protonmail.com>
2023-02-01 15:41:19 +0100

    Links and foot

---
+++ components/Now.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function Now() {
+	return process.env.NOW;
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const withMDX = require("@next/mdx")({
-	extension: /\.mdx$/,
+	extension: /\.mdx$/
 });
+
+const now = new Date();
 
 module.exports = withMDX({
 	reactStrictMode: true,
 	pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+	env: {
+		NOW: now.toLocaleString("en-US", {
+			month: "long",
+			year: "numeric"
+		})
+	}
 });
