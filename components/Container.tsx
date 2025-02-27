498b13638b0dd9bce7fdd183a8f1776b96a0f17b
Anthony Dugois <anthony.dugois@protonmail.com>
2025-02-27 09:54:06 +0100

    reset

--- components/Container.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export default function Container({ children }: { children: React.ReactChild }) {
-	return <div className="max-w-4xl lg:px-20">{children}</div>;
-}
