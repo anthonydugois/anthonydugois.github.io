229f787c5a186cfffc31280e1fec45c80ddbcddc
Anthony Dugois <anthony.dugois@protonmail.com>
2025-03-10 16:35:34 +0100

    update export process

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   env: {
